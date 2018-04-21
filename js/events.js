036fe6ea9215f9bb0a55b9da7d4601e948393cab
Shel Orock <sdo243@nyu.edu>
2018-04-21 19:00:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,13 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function*( {
+    if( click()) {
+      alert('Hey!');
+    }
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
